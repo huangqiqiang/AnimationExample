e42e913f331156a574ec3771fb836f4db187d610
linrenqiao <593979591@qq.com>
2018-01-03 18:03:39 +0800

    修改 : 主页

--- app/src/main/java/com/hqq/viewexample/MainActivity.kt
+++ app/src/main/java/com/hqq/viewexample/MainActivity.kt
@@ -1,17 +1,50 @@
 package com.hqq.viewexample
 
+import android.content.ComponentName
+import android.content.Context
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import com.hqq.viewexample.activity.HandwritingActivity
+import com.hqq.viewexample.activity.WaveActivity
+import com.hqq.viewexample.activity.animate.*
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    fun startActivity(cls: Class<*>) {
+        startActivity(Intent(this, cls))
+    }
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        button.setOnClickListener {
-            startActivity(Intent(this, HandwritingActivity::class.java))
+        btn_handWriting.setOnClickListener {
+            startActivity(HandwritingActivity::class.java)
+        }
+        btn_wave.setOnClickListener {
+            startActivity(WaveActivity::class.java)
+        }
+        btn_AnimatorSet.setOnClickListener {
+            startActivity(AnimatorSetActivity::class.java)
+        }
+        btn_Interpolator.setOnClickListener {
+            startActivity(InterpolatorActivity::class.java)
+        }
+        btn_ObjectAnimator.setOnClickListener {
+            startActivity(ObjectAnimatorActivity::class.java)
+        }
+        btn_PropertyValuesHolder.setOnClickListener {
+            startActivity(PropertyValuesHolderActivity::class.java)
+        }
+        btn_ValueAnimator.setOnClickListener {
+            startActivity(ValueAnimatorActivity::class.java)
+        }
+        btn_XmlAttributes.setOnClickListener {
+            startActivity(XmlAttributesActivity::class.java)
         }
     }
+
+
 }
