740f449627726cf08b377f743b06d58cd674a6fd
linrenqiao <593979591@qq.com>
2018-01-24 14:20:23 +0800

    剩下的 手势不知道怎么处理

--- app/src/main/java/com/hqq/viewexample/view/TempControlView.java
+++ app/src/main/java/com/hqq/viewexample/view/TempControlView.java
@@ -4,11 +4,15 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Path;
 import android.graphics.RectF;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.util.TypedValue;
+import android.view.MotionEvent;
 import android.view.View;
+
+import com.hqq.viewexample.Utils.LogUtils;
 
 /**
  * https://www.jianshu.com/p/2f7bfe1d7345
@@ -65,9 +69,19 @@ public class TempControlView extends View {
     private Paint titlePaint;
     // 文本提示
     private String title = "最高温度设置";
-
+    // 温度显示画笔
+    private Paint tempPaint;
     // 温度标识画笔
     private Paint tempFlagPaint;
+    // 每格的角度
+    private float angleOne = (float) 270 / (maxTemp - minTemp) / angleRate;
+
+    // 以下为旋转按钮相关
+
+    // 当前按钮旋转的角度
+    private float rotateAngle;
+    // 当前的角度
+    private float currentAngle;
 
     private void init() {
         dialPaint = new Paint();
@@ -101,6 +115,13 @@ public class TempControlView extends View {
         tempFlagPaint.setColor(Color.parseColor("#E4A07E"));
         tempFlagPaint.setStyle(Paint.Style.STROKE);
 
+        tempPaint = new Paint();
+        tempPaint.setAntiAlias(true);
+        tempPaint.setTextSize(sp2px(60));
+        tempPaint.setColor(Color.parseColor("#E27A3F"));
+        tempPaint.setTextAlign(Paint.Align.CENTER);
+        tempPaint.setStyle(Paint.Style.STROKE);
+
     }
 
 
@@ -123,8 +144,42 @@ public class TempControlView extends View {
         drawArc(canvas);
         drawText(canvas);
         drawRound(canvas);
+        drawButton(canvas);
+        drawTemp(canvas);
 
+    }
 
+    private void drawTemp(Canvas canvas) {
+        canvas.save();
+        canvas.translate(getWidth() / 2, getHeight() / 2);
+        float tempHeighe = (-tempPaint.ascent() - tempPaint.descent()) / 2;
+        canvas.drawText(minTemp + "°", 0, tempHeighe, tempPaint);
+        canvas.restore();
+
+    }
+
+    /**
+     * 绘制三角形 按钮
+     *
+     * @param canvas
+     */
+    private void drawButton(Canvas canvas) {
+        canvas.save();
+        canvas.translate(getWidth() / 2, getHeight() / 2);
+        LogUtils.e("angle   " + currentAngle);
+
+        float rorate= -135;
+        if (currentAngle != 0){
+            rorate= currentAngle +90;
+        }
+
+        canvas.rotate( rorate);
+        Path path = new Path();
+        path.moveTo(getX() / 2, -arcRadius + dp2px(5));
+        path.lineTo(getX() / 2 - arcRadius + dp2px(20) * 2, 0);
+        path.lineTo(getX() / 2 + arcRadius - dp2px(20) * 2, 0);
+        canvas.drawPath(path, raoudPaint);
+        canvas.restore();
     }
 
     /**
@@ -136,6 +191,7 @@ public class TempControlView extends View {
         canvas.save();
         canvas.drawCircle(getWidth() / 2, getHeight() / 2, arcRadius - dp2px(20), raoudPaint);
         canvas.restore();
+
     }
 
     /**
@@ -159,7 +215,6 @@ public class TempControlView extends View {
         canvas.rotate(-100, 0, 0);
         canvas.drawText(maxTemp + "", 0, dialRadius + (scaleHeight * 2) + dp2px(10), tempFlagPaint);
         canvas.restore();
-
     }
 
     /**
@@ -192,8 +247,7 @@ public class TempControlView extends View {
             canvas.rotate(4.5f);
         }
         canvas.rotate(90);
-        dialPaint.setColor(Color.parseColor("#E37364"));
-        canvas.drawLine(0, -dialRadius, 0, -dialRadius + scaleHeight, dialPaint);
+
 
         dialPaint.setColor(Color.parseColor("#E37364"));
         for (int i = 0; i < (temperature - minTemp) * angleRate; i++) {
@@ -203,6 +257,108 @@ public class TempControlView extends View {
         canvas.restore();
     }
 
+    private boolean isDown;
+    private boolean isMove;
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                isDown = true;
+                float downX = event.getX();
+                float downY = event.getY();
+
+                break;
+
+            case MotionEvent.ACTION_MOVE:
+                isMove = true;
+                float targetX;
+                float targetY;
+                downX = targetX = event.getX();
+                downY = targetY = event.getY();
+                float angle = calcAngle(targetX, targetY);
+
+                // 滑过的角度增量
+                float angleIncreased = angle - currentAngle;
+                // 防止越界
+                if (angleIncreased < -270) {
+                    angleIncreased = angleIncreased + 360;
+                } else if (angleIncreased > 270) {
+                    angleIncreased = angleIncreased - 360;
+                }
+                LogUtils.e(" x = " + targetX + " y = " + targetY);
+                IncreaseAngle(angleIncreased);
+                currentAngle = angle;
+                invalidate();
+                break;
+            case MotionEvent.ACTION_CANCEL:
+            case MotionEvent.ACTION_UP:
+                if (isDown && isMove) {
+                    // 纠正指针位置
+                    rotateAngle = (float) ((temperature - minTemp) * angleRate * 4.5);
+                    invalidate();
+                    isDown = false;
+                    isMove = false;
+                }
+                break;
+        }
+
+
+        return true;
+    }
+
+    /**
+     * 以按钮圆心为坐标圆点，建立坐标系，求出(targetX, targetY)坐标与x轴的夹角
+     *
+     * @param targetX x坐标
+     * @param targetY y坐标
+     * @return (targetX, targetY)坐标与x轴的夹角
+     */
+    private float calcAngle(float targetX, float targetY) {
+        float x = targetX - getWidth() / 2;
+        float y = targetY - getHeight() / 2;
+        double radian;
+        if (x != 0) {
+            float tan = Math.abs(y / x);
+            if (x > 0) {
+                if (y >= 0) {
+                    radian = Math.atan(tan);
+                } else {
+                    radian = 2 * Math.PI - Math.atan(tan);
+                }
+            } else {
+                if (y >= 0) {
+                    radian = Math.PI - Math.atan(tan);
+                } else {
+                    radian = Math.PI + Math.atan(tan);
+                }
+            }
+        } else {
+            if (y > 0) {
+                radian = Math.PI / 2;
+            } else {
+                radian = -Math.PI / 2;
+            }
+        }
+        return (float) ((radian * 180) / Math.PI);
+    }
+
+
+    /**
+     * 增加旋转角度
+     *
+     * @param angle 增加的角度
+     */
+    private void IncreaseAngle(float angle) {
+        rotateAngle += angle;
+        if (rotateAngle < 0) {
+            rotateAngle = 0;
+        } else if (rotateAngle > 270) {
+            rotateAngle = 270;
+        }
+        // 加上0.5是为了取整时四舍五入
+        temperature = (int) ((rotateAngle / angleOne) / angleRate + 0.5) + minTemp;
+    }
 
     public int dp2px(float dp) {
         return (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp,
--- app/src/test/java/com/hqq/viewexample/ExampleUnitTest.java
+++ app/src/test/java/com/hqq/viewexample/ExampleUnitTest.java
@@ -1,5 +1,7 @@
 package com.hqq.viewexample;
 
+import android.graphics.Point;
+
 import com.hqq.viewexample.Utils.LogUtils;
 
 import org.junit.Test;
@@ -14,24 +16,48 @@ import static org.junit.Assert.*;
 public class ExampleUnitTest {
     @Test
     public void addition_isCorrect() throws Exception {
-        assertEquals(4, 2 + 2);
+        //  assertEquals(4, 2 + 2);
 
-        LogUtils.e(new A().getA());
+        //   LogUtils.e(new A().getA());
+
+
+        System.out.println("------ " + (Math.atan2(4, 1) * 180 / Math.PI));
+        System.out.println("------ " + (Math.atan(4 / 1) * 180 / Math.PI));
+        System.out.println("------------ " + Math.atan(Math .abs(4/1)) / Math.PI * 180);
+
+        System.out.println(calcAngle(4,1));
+
+
 
     }
 
-
-
-    class A {
-        int  a;
-
-        public int getA() {
-            return a;
+    private float calcAngle(float targetX, float targetY) {
+        float x = targetX ;
+        float y = targetY ;
+        double radian;
+        if (x != 0) {
+            float tan = Math.abs(y / x);
+            if (x > 0) {
+                if (y >= 0) {
+                    radian = Math.atan(tan);
+                } else {
+                    radian = 2 * Math.PI - Math.atan(tan);
+                }
+            } else {
+                if (y >= 0) {
+                    radian = Math.PI - Math.atan(tan);
+                } else {
+                    radian = Math.PI + Math.atan(tan);
+                }
+            }
+        } else {
+            if (y > 0) {
+                radian = Math.PI / 2;
+            } else {
+                radian = -Math.PI / 2;
+            }
         }
-
-        public void setA(int a) {
-            this.a = a;
-        }
+        return (float) ((radian * 180) / Math.PI);
     }
 
 }
